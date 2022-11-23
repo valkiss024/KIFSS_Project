83219a96be3b7fbfc8685fb87bbf37b1ee42e565
ShaunLen <shaunlen99@gmail.com>
2022-11-23 23:21:37 +0000

    Added automatic insert to DB of Triggers/SelfCheck

--- dashboard/inserts.py
+++ dashboard/inserts.py
@@ -1,10 +1,14 @@
 from dashboard import db
-from dashboard.models import Trigger, SelfCheck
+from dashboard.models import Trigger, SelfCheck, Sensor
+from geopy.geocoders import Nominatim
 import json, os
 import datetime
 
 triggers = os.path.abspath('dashboard/data/trigger')
 selfchecks = os.path.abspath('dashboard/data/selfcheck')
+
+engine = db.create_engine("mysql+pymysql://root@localhost/kifss")
+connection = engine.connect()
 
 def json_to_sql():
 
@@ -16,10 +20,16 @@ def json_to_sql():
             serial_number = data.get("Serial number")
             status = data.get("Status")
             date = data.get("Date")
+            date = datetime.datetime.fromtimestamp(date/1000)
 
-            new_trigger = Trigger(serial_number=serial_number, status=status, date=datetime.datetime.fromtimestamp(date/1000), assessed=0)
+            sensor = connection.execute('SELECT * FROM Sensor WHERE serial_number="' + serial_number + '"')
 
-            db.session.add(new_trigger)
+            for row in sensor:
+                new_trigger = Trigger(serial_number=serial_number, status=status, date=date, assessed=0)
+                exists = db.session.query(Trigger).filter_by(serial_number=serial_number, date=date).first()
+                if not exists:
+                    db.session.add(new_trigger)
+                
             db.session.commit()
 
         except FileNotFoundError:
@@ -33,11 +43,38 @@ def json_to_sql():
             serial_number = data.get("Serial number")
             status = data.get("Status")
             date = data.get("Date")
+            date = datetime.datetime.fromtimestamp(date/1000)
 
-            new_selfcheck = SelfCheck(serial_number=serial_number, status=status, date=datetime.datetime.fromtimestamp(date/1000), assessed=0)
+            sensor = connection.execute('SELECT * FROM Sensor WHERE serial_number="' + serial_number + '"')
 
-            db.session.add(new_selfcheck)
+            for row in sensor:
+                new_selfcheck = SelfCheck(serial_number=serial_number, status=status, date=date, assessed=0)
+                exists = db.session.query(SelfCheck).filter_by(serial_number=serial_number, date=date).first()
+                if not exists:
+                    db.session.add(new_selfcheck)
+
             db.session.commit()
 
         except FileNotFoundError:
             continue
+        
+
+def add_example_sensors(organization):
+    sensors = []
+    locations = [["187 Fairview Drive", "Aberdeen", "Aberdeenshire"], ["191 Fairview Drive", "Aberdeen", "Aberdeenshire"], ["197 Fairview Drive", "Aberdeen", "Aberdeenshire"],
+                 ["215 Fairview Drive", "Aberdeen", "Aberdeenshire"], ["71 Fairview Drive", "Aberdeen", "Aberdeenshire"], ["21 Tillydrone Avenue", "Aberdeen", "Aberdeenshire"],
+                 ["9 Merrivale, Station Road", "Dyce", "Aberdeenshire"], ["5 Merrivale, Station Road", "Dyce", "Aberdeenshire"], ["7 Merrivale, Station Road", "Dyce", "Aberdeenshire"],
+                 ["14 Trinity Court", "Westhill", "Aberdeenshire"]]
+    locator = Nominatim(user_agent="kifss")
+
+    for i in range(len(locations)):
+        loc = locator.geocode(locations[i][0] + "," + locations[i][1] + "," + locations[i][2])
+        if loc is not None:
+            sensors.append(Sensor("A00000000" + str(i), "Flat 1", "Pass", datetime.datetime.now(), organization, locations[i][0], locations[i][1], locations[i][2], loc.latitude, loc.longitude))
+        else:
+            sensors.append(Sensor("A00000000" + str(i), "Flat 1", "Pass", datetime.datetime.now(), organization, locations[i][0], locations[i][1], locations[i][2], "", ""))
+    
+    for i in sensors:
+        db.session.merge(i)
+    
+    db.session.commit()
