223d2b04cf6c1d68e4b01276093d82ab40768a87
ShaunLen <shaunlen99@gmail.com>
2022-11-24 14:50:57 +0000

    Switched from sqlite to mysql in settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -2,8 +2,8 @@ import os
 
 SECRET_KEY = 'secret'  # Reading in the SECRET_KEY environmental variable
 
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/kifss'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.sqlite3'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/kifss'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///database.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Admin site configuration
