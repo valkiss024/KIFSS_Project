47dae9acfbd0cee13406c1add8d05b1dac03008c
arawee <53333722+arawee@users.noreply.github.com>
2022-11-24 23:32:13 +0000

    popup

--- dashboard/templates/js/printPDF.js
+++ dashboard/templates/js/printPDF.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function(){
            // console.log(base64image);
 
             let pdf = new jsPDF('p', 'px', [1920, 1440]);
-            pdf.addImage(base64image, 'PNG', 15, 15, 1812, 1130);
+            pdf.addImage(base64image, 'PNG', 15, 15, 1920, 1220);
             pdf.save('webtylepress-throw.pdf');
         });
     });
