c7178b0837ce15305680e9e41a492a86eb8c41bc
PluckyAurora <romekbardzikzomb@gmail.com>
2022-11-24 19:49:43 +0000

    better pdf resolution

--- dashboard/templates/js/printPDF.js
+++ dashboard/templates/js/printPDF.js
@@ -4,30 +4,9 @@ jQuery(document).ready(function(){
             let base64image = canvas.toDataURL('image/png');
            // console.log(base64image);
 
-            let pdf = new jsPDF('p', 'px', [1600, 1080]);
+            let pdf = new jsPDF('p', 'px', [1920, 1440]);
             pdf.addImage(base64image, 'PNG', 15, 15, 1812, 1130);
             pdf.save('webtylepress-throw.pdf');
         });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-// const printBtn = document.getElementById('exportbutton');
-
-// printBtn.addEventListener('click', function() {
-//     print();
-// });
-
-// let htmlPDF = document.getElementById("devices");
-// let exportPDF = document.getElementById("exportbutton");
-// exportPDF.onclick = (e) => html2pdf(htmlPDF);
-// exportPDF.onclick = (e) => html2pdf(base64image)
